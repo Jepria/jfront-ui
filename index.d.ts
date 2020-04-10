4661fbce97620f4d4454f33d0031fca5e27322b3
remitot <call9096463086@gmail.com>
2020-04-10 18:11:05 +0300

    Initial commit
    Works together with https://github.com/softspiders/cra-ie11-ts-starter (branch dev__component-import), using npmlink
    
    * Clone ComboBox component from https://github.com/Jepria/oauth/tree/client-react/module/OAuth/App/client-react/src/components/form/input into src/components
    * Add index.d.ts file (not working properly)

---
+++ index.d.ts
@@ -0,0 +1,10 @@
+declare module 'jfront-components' {
+  export {
+    ComboBoxOptionComponent as ComboBoxOption,
+    ComboBoxListComponent as ComboBoxList,
+    ComboBoxPopupComponent as ComboBoxPopup,
+    ComboBoxInputComponent as ComboBoxInput,
+    ComboBoxButtonComponent as ComboBoxButton,
+    ComboBoxComponent as ComboBox
+  }
+}
