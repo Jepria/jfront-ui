c316bdedfcef7288cd186e08fcd6e98c48d04237
alexsav94 <aleksoider@mail.ru>
2020-05-29 12:35:45 +0300

    feat: стили GWT грида.

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,11 @@ export * from './components/input/ComboBox';
 export * from './components/input/InputText';
 export * from './components/input/InputSelect';
 
-export * from './components/grid/GridBody';
-export * from './components/grid/GridHeader';
-export * from './components/grid/GridPagingBar';
-export * from './components/grid/GridRow';
-export * from './components/grid/GridTable';
-export * from './components/grid';
+export * from './components/grid/common/GridBody';
+export * from './components/grid/common/GridHeader';
+export * from './components/grid/common/GridPagingBar';
+export * from './components/grid/common/GridRow';
+export * from './components/grid/common/GridTable';
+export * from './components/grid/common';
+export * from './components/grid/jepgrid';
 export * from './utils';
