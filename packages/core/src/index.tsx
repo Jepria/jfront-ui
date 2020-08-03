43ceaab495aff82cf3b393ad9903f0c9cba66720
AlexanderLapygin <AlexanderLapygin@gmail.com>
2020-08-03 06:34:57 +0300

    refactor: removal of excess feat: update core index

--- packages/core/src/index.tsx
+++ packages/core/src/index.tsx
@@ -1,7 +1,7 @@
 export * from '@jfront-ui/app'
 export * from '@jfront-ui/datepicker'
 export * from '@jfront-ui/form'
-//export type { GridBody, GridHeader, GridPagingBar, GridRow, GridTable } from '@jfront-ui/grid'
+export * from '@jfront-ui/grid'
 export * from '@jfront-ui/hooks'
 //export * from '@jfront-ui/input'
 export * from '@jfront-ui/layout'
