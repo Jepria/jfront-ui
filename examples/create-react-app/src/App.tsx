773b4022c35d4aadf8ae2897b39ddb4107a810b1
AlexanderLapygin <AlexanderLapygin@gmail.com>
2020-08-07 21:59:49 +0300

    fix: copying images during babel transpilation feat: add components to example (GridTable still has problems though)

--- examples/create-react-app/src/App.tsx
+++ examples/create-react-app/src/App.tsx
@@ -1,6 +1,10 @@
 import * as React from "react"
 import { DatePicker } from "@jfront-ui/core"
 import {PagingToolBar} from "@jfront-ui/pagingbar";
+//import {GridTable} from "@jfront-ui/grid/common/GridTable";
+import {InputText} from "@jfront-ui/input";
+import {TabPanel} from "@jfront-ui/tabpanel";
+import {Toolbar} from "@jfront-ui/toolbar";
 
 function App() {
   const onChange = () => {console.log('onChange()')}
@@ -11,6 +15,14 @@ function App() {
       />
       <PagingToolBar
         pageCount={2}/>
+      {/*<GridTable*/}
+      {/*/>*/}
+      <InputText name="InputTextName"
+      />
+      <TabPanel
+      />
+      <Toolbar
+      />
     </div>
   )
 }
