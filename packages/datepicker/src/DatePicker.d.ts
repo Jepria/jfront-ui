9f9766a5c41b5bcb45bf033818d0225881ef8e82
AlexSav94 <aleksoider@gmail.com>
2020-08-19 10:49:47 +0300

    refactor: deleted old build files

--- packages/datepicker/src/DatePicker.d.ts
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { ReactDatePickerProps } from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
-interface DatePickerInterface extends ReactDatePickerProps {
-}
-export declare const DatePicker: React.FC<DatePickerInterface>;
-export {};
