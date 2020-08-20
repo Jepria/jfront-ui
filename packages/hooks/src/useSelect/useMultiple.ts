aa9bc4b4e67f3539519eef5e30235cbe065cbf69
AlexSav94 <aleksoider@gmail.com>
2020-08-20 12:50:00 +0300

    fix: error fixes and refactoring

--- packages/hooks/src/useSelect/useMultiple.ts
+++ packages/hooks/src/useSelect/useMultiple.ts
@@ -1,112 +1,72 @@
-import { UseSelectInstance, equals } from ".";
+import { UseSelectInstance, equals } from "."
 
 export interface UseMultipleInstance extends UseSelectInstance {
-  isSelectedAll: () => boolean;
-  selectAll: () => void;
+  isSelectedAll: () => boolean
+  selectAll: () => void
 }
 
 export function useMultiple(instance: UseSelectInstance) {
-
-  instance.hooks.useInstance = instance.hooks.useInstance ? [...instance.hooks.useInstance, useInstance] : [useInstance];
-
+  instance.hooks.useInstance = instance.hooks.useInstance
+    ? [...instance.hooks.useInstance, useInstance]
+    : [useInstance]
 }
 
 function useInstance(instance: UseMultipleInstance) {
-
-  const getSelectedOption = (): Array<any>  => {
-    const {
-      state
-    } = instance;
-
-    if (state?.value) {
-      return state.value
-    } else {
-      return [];
-    }
-  };
-
-  const mapArray = (options: Array<any>) => {
-    const {
-      props
-    } = instance;
-
-    return options.map((option: any) => {
-      if (props.getOptionValue) {
-        return props.getOptionValue(option)
-      } else {
-        return option.value;
-      }
-    })
-  }
-
-  const getValue = (): Array<any> => {
-    const {
-      state,
-      props
-    } = instance;
-
-    if (state?.value) {
-      return mapArray(state.value);
-    } else {
-      return [];
-    }
-  }
-
   const selectOption = (value: any) => {
-
-    const {
-      dispatch,
-      props
-    } = instance;
+    const { dispatch, props, state } = instance
 
     if (Array.isArray(value)) {
-      if (dispatch) dispatch({ type: 'select', value: [...value] });
+      if (dispatch) dispatch({ type: "select", value: [...value] })
       if (instance.props.onChange) {
-        instance.props.onChange(mapArray(value));
+        instance.props.onChange(value)
       }
     } else if (value) {
-      let newSelected;
-      let optionValue = props.getOptionValue ? props.getOptionValue(value) : (value).value;
-      let values = getValue();
-      let options = getSelectedOption();
+      let newSelected
+      const optionValue = props.getOptionValue
+        ? props.getOptionValue(value)
+        : value.value
+      const values = state?.value
       if (values && values.includes(optionValue)) {
-        newSelected = options.slice();
-        newSelected.splice(values.indexOf(optionValue), 1);
+        newSelected = values.filter((item: any) => item !== optionValue)
       } else {
-        if (options) {
-          newSelected = [...options, value]
+        if (values) {
+          newSelected = [...values, optionValue]
         } else {
-          newSelected = [value];
+          newSelected = [optionValue]
         }
       }
-      if (dispatch) dispatch({ type: 'select', value: newSelected });
+      if (dispatch) dispatch({ type: "select", value: newSelected })
       if (props.onChange) {
-        props.onChange(newSelected.map((option: any) => {
-          if (props.getOptionValue) {
-            return props.getOptionValue(option)
-          } else {
-            return option.value
-          }
-        }));
+        props.onChange(newSelected)
       } else {
-        if (dispatch) dispatch({ type: 'select', value: [] });
+        if (dispatch) dispatch({ type: "select", value: [] })
         if (instance.props.onChange) {
-          instance.props.onChange([]);
+          instance.props.onChange([])
         }
       }
     }
-
   }
 
-  instance.selectOption = selectOption;
+  instance.selectOption = selectOption
 
-  instance.getSelectedValue = () => getValue();
-
-  instance.getSelectedOption = () => getSelectedOption();
-
-  instance.selectAll = () => selectOption(instance.props.options);
+  instance.selectAll = () =>
+    selectOption(
+      instance.props.options.map((option) =>
+        instance.props.getOptionValue
+          ? instance.props.getOptionValue(option)
+          : option.value,
+      ),
+    )
 
   instance.isSelectedAll = () => {
-    return equals(instance.props.options.map(option => instance.props.getOptionValue ? instance.props.getOptionValue(option) : option.value), getValue());
+    const { state } = instance
+    return equals(
+      instance.props.options.map((option) =>
+        instance.props.getOptionValue
+          ? instance.props.getOptionValue(option)
+          : option.value,
+      ),
+      state?.value,
+    )
   }
 }
