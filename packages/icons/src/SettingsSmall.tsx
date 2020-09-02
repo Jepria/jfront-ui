e05bda77542c2c74e176638276dc5ccf35ab7134
AlexSav94 <aleksoider@gmail.com>
2020-09-02 09:41:22 +0300

    feat: icons

---
+++ packages/icons/src/SettingsSmall.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+export const SettingsSmallImage = (
+  props: React.HTMLAttributes<HTMLImageElement>,
+) => {
+  return (
+    <img
+      {...props}
+      src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAABGdBTUEAAK/INwWK6QAAABl0RVh0U29mdHdhcmUAQWRvYmUgSW1hZ2VSZWFkeXHJZTwAAADoSURBVHjaXFC7iYVAFD2zaAlGIhoIZhpYghYgWIy1WIRgZCRoAyYGgpiIYAEmiijMvjO7K2/fwP3Mued+RZ7n+Hjy14p38Ivqvm/pOI6kjeMYlHeMHI3Ktm24rqvsdV2qQhRF0HVd/dd1hbZtGyiWZSmgLEtFTJIEpmliGIaf1ud5yjRNcRwHmqZBlmWCQp8YY+RoL4VlWVSWYRjP8PRJZIwcVhRVVT3ZdV1Lyl8Xxl5jCY1L7PsOz/PQ973amI++7/sYxxHP1tM0oSgKzPP8bN22rcKJBUEA8XnwruvU3cIw/HfwbwEGAKbej5/tP3njAAAAAElFTkSuQmCC"
+    />
+  )
+}
