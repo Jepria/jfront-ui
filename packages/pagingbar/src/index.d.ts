9f2379d3bc27be399ca4fac7b93f75e59dd072bc
AlexSav94 <aleksoider@gmail.com>
2020-09-02 09:44:50 +0300

    fix: icons

--- packages/pagingbar/src/index.d.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-interface PagingToolBarProps {
-    startPageNumber?: number;
-    pageCount: number;
-    onChange?: (currentPageNumber: number) => void;
-}
-export declare const PagingToolBar: React.FC<PagingToolBarProps>;
-export {};
