2e3d5624420ed51b49a6430ca7b15677953cf6ab
AlexSav94 <aleksoider@gmail.com>
2020-09-02 09:48:52 +0300

    chore: removed unnecessary module

--- packages/table/src/react-app-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react-scripts" />
-declare module "*.png" {
-  const value: any
-  export default value
-}
-declare module "*.gif" {
-  const value: any
-  export default value
-}
