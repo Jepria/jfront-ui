47f87f74ef91ac23ae65bc5ed72963ec9d76027e
AlexSav94 <aleksoider@gmail.com>
2020-09-10 17:55:48 +0300

    fix: replaceAll changed to replace test: mock for css

--- packages/datepicker/jest.config.js
+++ packages/datepicker/jest.config.js
@@ -1,8 +1,11 @@
 module.exports = {
   preset: "ts-jest",
-  testEnvironment: "node",
+  testEnvironment: "jsdom",
   collectCoverageFrom: ["tests/**/*.{ts,tsx,js,jsx}"],
   transform: { ".(ts|tsx)$": "ts-jest/dist" },
   transformIgnorePatterns: ["[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$"],
   setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
+  moduleNameMapper: {
+    "\\.(css)": "<rootDir>/_mocks_/styleMock.js",
+  },
 }
