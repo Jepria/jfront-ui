633176eba7fb00369db46699ad1b25301d180c4d
dmitrii92 <fogot92@gmail.com>
2020-09-10 17:11:49 +0300

    fix: delete bg.png

--- packages/tabpanel/src/TabPanel.tsx
+++ packages/tabpanel/src/TabPanel.tsx
@@ -1,13 +1,12 @@
-import styled from 'styled-components';
-import bg from './images/bg.png'
-import React from "react";
+import styled from "styled-components"
+import React from "react"
 
 const TabPanel = styled.div`
-  height:22px;
+  height: 22px;
   font-family: tahoma, arial, helvetica, sans-serif;
   color: rgb(21, 66, 139);
   font-size: 11px;
-`;
+`
 
 interface TabProps {
   selected?: boolean
@@ -15,30 +14,29 @@ interface TabProps {
 
 const Tab = styled.div<TabProps>`
   display: inline-block;
-  height:16px;
-  text-align:center;
+  height: 16px;
+  text-align: center;
   margin-left: 2px;
   padding: 3px 6px 3px 6px;
   min-width: 20px;
-  border: 1px solid #8DB2E3;
+  border: 1px solid #8db2e3;
   border-top-left-radius: 3px;
   border-top-right-radius: 3px;
-  border-bottom-color: #D7E4F3;
-  ${props => props.selected ? `
+  border-bottom-color: #d7e4f3;
+  ${(props) =>
+    props.selected
+      ? `
   background-color: #D7E4F3;
     cursor: default;
     font-weight: bold;
-  ` : `
-    background-image: url(${bg});
-    background-color: white;
-    background-repeat: repeat;
-    background-position: 0 100%;
+  `
+      : `
+    background: linear-gradient(#dae6f4, #d0def0);
     cursor: pointer;
     &:hover{
     opacity: 0.8
   }
-  `
-}
-`;
+  `}
+`
 
-export {TabPanel, Tab};
+export { TabPanel, Tab }
