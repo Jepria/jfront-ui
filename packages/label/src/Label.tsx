cb0183bf4750143ddf19af92a5859b921e95cec3
AlexSav94 <aleksoider@gmail.com>
2020-09-14 09:48:07 +0300

    fix: label refactor

--- packages/label/src/Label.tsx
+++ packages/label/src/Label.tsx
@@ -1,16 +1,25 @@
-import React from "react"
+import React, { RefObject } from "react"
 import styled from "styled-components"
 
-interface LabelInterface extends React.LabelHTMLAttributes<HTMLLabelElement> {}
+export interface LabelProps
+  extends React.LabelHTMLAttributes<HTMLLabelElement> {
+  ref?: RefObject<HTMLLabelElement>
+}
 
 const StyledLabel = styled.label`
-  display: inline-block;
-  text-align: right;
-  width: 200px;
-  min-width: 200px;
+  position: relative;
+  display: -webkit-inline-box;
+  display: -ms-inline-flexbox;
+  display: inline-flex;
   padding-right: 5px;
+  font-family: tahoma, arial, helvetica, sans-serif;
+  font-size: 12px;
+  float: left;
+  word-break: break-word;
+  justify-content: flex-end;
+  text-align: right;
 `
 
-export const Label: React.FC<LabelInterface> = (props) => {
+export const Label: React.FC<LabelProps> = (props) => {
   return <StyledLabel {...props}>{props.children}</StyledLabel>
 }
