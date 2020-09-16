06facb022e698230fe1d776609c0514fa528d24b
AlexSav94 <aleksoider@gmail.com>
2020-09-16 15:01:27 +0300

    fix: masked input and datepicker fixes

--- packages/datepicker/src/DatePicker.tsx
+++ packages/datepicker/src/DatePicker.tsx
@@ -2,20 +2,28 @@ import React from "react"
 import ReactDatePicker, { ReactDatePickerProps } from "react-datepicker"
 import "react-datepicker/dist/react-datepicker.css"
 import { ru } from "date-fns/locale"
-import { MaskedTextInput, InputProps } from "@jfront/ui-input"
+import {
+  TextInput,
+  InputProps,
+  parseMask,
+  parsePlaceholderFromString,
+} from "@jfront/ui-input"
+import MaskedInput from "react-text-mask"
 
 export const dateFormatToMask = (dateFormat: string | string[]) => {
   if (Array.isArray(dateFormat)) {
-    return dateFormat.map((format) => format.replace(/[mMdDyYhH]/g, "9"))
+    return dateFormat
+      .map((format) => format.replace(/[mMdDyYhH]/g, "9"))
+      .join("")
   } else {
     return dateFormat.replace(/[mMdDyYhH]/g, "9")
   }
 }
 
 export const DatePicker = React.forwardRef<
-  HTMLInputElement,
+  ReactDatePicker,
   ReactDatePickerProps & InputProps
->((props, ref) => {
+>(({ onChange, onSelect, ...props }, ref) => {
   const {
     peekNextMonth = true,
     showMonthDropdown = true,
@@ -25,13 +33,13 @@ export const DatePicker = React.forwardRef<
     autoComplete = "off",
     locale = ru,
     customInput = (
-      <MaskedTextInput
-        mask={dateFormatToMask(dateFormat)}
-        maskPlaceholder="*"
-        error={props.error}
-        isLoading={props.isLoading}
-        alwaysShowMask
-        ref={ref}
+      <MaskedInput
+        placeholderChar={"*"}
+        {...props}
+        mask={parseMask(dateFormatToMask(dateFormat))}
+        render={(innerRef, props) => (
+          <TextInput inputRef={innerRef} {...props} />
+        )}
       />
     ),
   } = props
@@ -46,7 +54,12 @@ export const DatePicker = React.forwardRef<
       dropdownMode={dropdownMode}
       dateFormat={dateFormat}
       autoComplete={autoComplete}
+      onChange={onChange}
+      onSelect={onSelect}
       locale={locale}
+      isClearable={!props.error && !props.isLoading}
+      placeholderText={parsePlaceholderFromString(dateFormatToMask(dateFormat))}
+      ref={ref}
     />
   )
 })
