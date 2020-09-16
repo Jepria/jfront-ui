f9f1f8347a3f32e8b48a87ba22dfb3f224077d36
Dmitriy Volkov <fogot92@gmail.com>
2020-09-16 16:54:28 +0300

    Revert "fix: delete bg.png"
    This reverts commit 633176eba7fb00369db46699ad1b25301d180c4d.

--- packages/tabpanel/stories/tabpanel.stories.tsx
+++ packages/tabpanel/stories/tabpanel.stories.tsx
@@ -1,18 +1,20 @@
-import * as React from "react"
-import { Tab, TabPanel } from "../src/TabPanel"
+import * as React from 'react'
+import { TabPanel } from '../src/TabPanel'
 
 export default {
-  title: "TabPanel",
-  decorators: [(StoryFn: Function) => <StoryFn />],
+  title: 'TabPanel',
+  decorators: [
+    (StoryFn: Function) => (
+    <StoryFn />
+    ),
+  ],
 }
 
 export const BasicUsage = () => {
   return (
     <>
-      <TabPanel>
-        <Tab>Tab1</Tab>
-        <Tab>Tab2</Tab>
-      </TabPanel>
+      <TabPanel
+      />
     </>
   )
 }
