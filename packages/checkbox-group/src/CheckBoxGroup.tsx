66a26b3cafd01e55a3d41345f8c508f183d4ea04
Dmitriy Volkov <fogot92@gmail.com>
2020-09-16 16:54:28 +0300

    Revert "fix: useEffect added"
    This reverts commit 32d53b5c411ae3e46595cc5e2064a533c688ae4b.

--- packages/checkbox-group/src/CheckBoxGroup.tsx
+++ packages/checkbox-group/src/CheckBoxGroup.tsx
@@ -1,4 +1,4 @@
-import React, { useState, ReactNode, useEffect } from "react"
+import React, { useState, ReactNode } from "react"
 import styled from "styled-components"
 import { LoadingImage, ExclamationImage } from "@jfront/ui-icons"
 
