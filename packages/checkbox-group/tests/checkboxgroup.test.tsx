fddae0b2f96413b0533e905efcefc1d08392a495
dmitrii92 <fogot92@gmail.com>
2020-10-28 16:05:28 +0300

    fix: upgrade test. Warning: layout.test.tsx deleted!

--- packages/checkbox-group/tests/checkboxgroup.test.tsx
+++ packages/checkbox-group/tests/checkboxgroup.test.tsx
@@ -6,7 +6,7 @@ import { CheckBox } from "../../checkbox/src"
 test("Checking for the existence of an element CheckBoxGroup", () => {
   expect(
     render(
-      <CheckBoxGroup name="name" text="Group" isLoading={false}>
+      <CheckBoxGroup name="name" isLoading={false}>
         <CheckBox disabled={true} label="label1" value="value1" />
         <CheckBox label="label2" value="value2" />
         <CheckBox label="label3" value="value3" />
--- packages/form/tests/form.test.tsx
+++ packages/form/tests/form.test.tsx
@@ -1,8 +1,7 @@
 import { render } from "@testing-library/react"
 import React from "react"
-import { Form, FormField } from "../src"
+import { Form } from "../src"
 
 test("Checking for the existence of an element Form, FormField", () => {
   render(<Form />)
-  render(<FormField />)
 })
