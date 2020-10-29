f8a5160fda0758f084fde17b27e6817dc99eb31c
Kazeev <misha_kazeev@mail.ru>
2020-10-30 00:23:53 +0300

    test: added simple validation of time input in datepicker

--- packages/datepicker/test/datepicker.test.tsx
+++ packages/datepicker/test/datepicker.test.tsx
@@ -1,12 +1,30 @@
-import { render } from "@testing-library/react"
+import { fireEvent, render, screen } from "@testing-library/react"
 import React from "react"
 import { DatePicker } from "../src"
+import { ComboBox, ComboBoxItem } from "@jfront/ui-combobox/src"
 
 const date = null
 const onChange = () => {}
 
 test("Checking for the existence of an element DatePicker", () => {
   render(<DatePicker onChange={onChange} />)
+})
+
+//todo make normal time type
+test("DatePicker data input ", () => {
+  let dateToCheck
+  render(
+    <DatePicker
+      data-testid="DatePickerInputCheck"
+      onChange={(name: Date) => {
+        dateToCheck = name
+      }}
+    />,
+  )
+  const input = screen.getByTestId("DatePickerInputCheck")
+  fireEvent.mouseEnter(input)
+  fireEvent.change(input, { target: { value: "2020-05-10" } })
+  expect(dateToCheck).toEqual(new Date("2020-05-09T21:00:00.000Z")) //Дата при задавание в поле input из "2020-05-10" из за часовых часов превращается в "2020-05-09T21:00:00.000Z"//todo сделать нормально
 })
 
 test("Matches snapshot ", () => {
