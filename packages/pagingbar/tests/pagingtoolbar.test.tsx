d2fa77ab0ffed8f9252f10d8f9e7db2f162a5bf1
Kazeev <misha_kazeev@mail.ru>
2020-11-24 16:11:12 +0300

    test: added behavior test pagingtoolbar

--- packages/pagingbar/tests/pagingtoolbar.test.tsx
+++ packages/pagingbar/tests/pagingtoolbar.test.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { render } from "@testing-library/react"
+import { fireEvent, render, screen } from "@testing-library/react"
 import { PagingToolBar } from "../src"
 
 test("Checking for the existence of an element PagingToolBar", () => {
@@ -9,4 +9,19 @@ test("Checking for the existence of an element PagingToolBar", () => {
 test("Matches snapshot ", () => {
   const { asFragment } = render(<PagingToolBar pageCount={2} />)
   expect(asFragment()).toMatchSnapshot()
+})
+test("Enter values ​​in a string field", () => {
+  let itemSelection
+  render(
+    <PagingToolBar
+      onChange={(currentPageNumber) => {
+        itemSelection = currentPageNumber
+      }}
+      pageCount={6}
+    />,
+  )
+
+  const input = screen.getAllByRole("spinbutton")[0]
+  fireEvent.click(screen.getAllByRole("button")[2])
+  expect(itemSelection).toEqual(2)
 })
