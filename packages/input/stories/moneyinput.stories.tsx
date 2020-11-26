07dfb50c8fd94af97cb7fb1400faadb44e788bac
dmitrii92 <fogot92@gmail.com>
2020-11-26 16:22:50 +0300

    chore: add story for moneyinput

--- packages/input/stories/moneyinput.stories.tsx
+++ packages/input/stories/moneyinput.stories.tsx
@@ -1,5 +1,7 @@
 import * as React from "react"
 import { MoneyInput } from "../src"
+import { useState } from "react"
+import { NumberFormatValues } from "react-number-format"
 
 export default {
   title: "MoneyInput",
@@ -32,6 +34,29 @@ export const Error = () => {
   return (
     <>
       <MoneyInput name="InputTextNameLoading" error="wrong value" />
+    </>
+  )
+}
+
+export const ClearValue = () => {
+  const [val, setVal] = useState("")
+  return (
+    <>
+      <MoneyInput
+        name="InputTextNameLoading"
+        error="wrong value"
+        value={val}
+        onValueChange={(values: NumberFormatValues) => {
+          setVal(values.value)
+        }}
+      />
+      <button
+        onClick={() => {
+          setVal("")
+        }}
+      >
+        Clean
+      </button>
     </>
   )
 }
