9b3860222b3d16439aaf2ba8610e6012749deed9
dmitrii92 <fogot92@gmail.com>
2020-12-11 16:31:07 +0300

    chore: combobox add disabled story

--- packages/combobox/stories/combobox.stories.tsx
+++ packages/combobox/stories/combobox.stories.tsx
@@ -161,3 +161,21 @@ export const Error = () => {
     </>
   )
 }
+
+export const Disabled = () => {
+  return (
+    <>
+      <ComboBox style={{ width: "200px" }} disabled>
+        <ComboBoxItem value={1} label="test1" />
+        <ComboBoxItem value={2} label="test2" />
+        <ComboBoxItem value={3} label="test3" />
+        <ComboBoxItem value={4} label="test4" />
+        <ComboBoxItem value={5} label="test5" />
+        <ComboBoxItem value={6} label="test6" />
+        <ComboBoxItem value={7} label="test7" />
+        <ComboBoxItem value={8} label="test8" />
+        <ComboBoxItem value={9} label="test9" />
+      </ComboBox>
+    </>
+  )
+}
