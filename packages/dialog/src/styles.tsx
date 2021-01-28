2d44994fa570c5c1e9f225b5066a81993acf0e73
AlexSav94 <aleksoider@gmail.com>
2021-01-28 12:32:20 +0300

    fix: dialog props and styles fixes

--- packages/dialog/src/styles.tsx
+++ packages/dialog/src/styles.tsx
@@ -1,23 +1,28 @@
-import styled from "styled-components";
-import { ErrorImage, WarningImage } from '@jfront/ui-icons';
+import styled from "styled-components"
+import { ErrorImage, WarningImage } from "@jfront/ui-icons"
 import { Button } from "@jfront/ui-button"
+import { Modal } from "@jfront/ui-modal"
 
 export const StyledButton = styled(Button)`
   margin: 5px;
   padding: 3px 5px;
-`;
+`
 
 export const FieldSet = styled.fieldset`
-  border: 1px solid #99BBE8;
+  border: 1px solid #99bbe8;
   margin: 5px;
-  padding: .625em;
-`;
+  padding: 0.625em;
+`
+
+export const StyledModal = styled(Modal)`
+  max-height: 90%;
+`
 
 export const Legend = styled.legend`
-  color: #15428B;
+  color: #15428b;
   font-size: 11px;
   font-weight: bold;
-`;
+`
 
 export const TextArea = styled.textarea`
   font-size: 11px;
@@ -26,17 +31,19 @@ export const TextArea = styled.textarea`
   height: 100%;
   width: 100%;
   overflow: auto;
-`;
+  min-height: 20em;
+  min-width: 20em;
+`
 
 export const Error = styled(ErrorImage)`
   height: 32px;
   width: 32px;
-`;
+`
 
 export const Warning = styled(WarningImage)`
   height: 32px;
   width: 32px;
-`;
+`
 
 export const ImageBox = styled.div`
   box-sizing: border-box;
