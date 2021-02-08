f60021df068bd1aae9fd36c961185acd89a196b3
AlexSav94 <aleksoider@gmail.com>
2021-02-09 00:27:37 +0300

    fix: storybook IE 11

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,35 +1,34 @@
-const path = require("path")
-
 module.exports = {
-  stories: ["../packages/**/stories/*.stories.tsx"],
-  addons: [
-    "storybook-addon-performance/register",
-    "@storybook/addon-a11y",
-    "@storybook/addon-storysource",
-  ],
-  typescript: {
-    reactDocgen: false,
-  },
-  webpackFinal: async (config, { configType }) => {
-    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
-    // You can change the configuration based on that.
-    // 'PRODUCTION' is used when building the static version of storybook.
-
-    // Make whatever fine-grained changes you need
-    config.module.rules.push({
-      test: /\.less$/,
-      use: [
-        require.resolve("style-loader"),
+  stories: ["../packages/**/stories/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [],
+  webpackFinal: async (config, { configType }) => ({
+    ...config,
+    module: {
+      ...config.module,
+      rules: [
+        ...config.module.rules.slice(1),
         {
-          loader: require.resolve("css-loader"),
-          options: { modules: false },
+          test: /\.(ts|js|jsx|tsx?)$/,
+          use: [
+            {
+              loader: "babel-loader",
+              options: {
+                exclude: [
+                  // \\ for Windows, / for macOS and Linux
+                  /node_modules[\\/]core-js/,
+                  /node_modules[\\/]webpack[\\/]buildin/,
+                ],
+                presets: ["@babel/preset-env"],
+                plugins: ["@babel/plugin-proposal-class-properties"],
+              },
+            },
+          ],
         },
-        require.resolve("less-loader"),
       ],
-      include: path.resolve(__dirname, "../"),
-    })
-
-    // Return the altered config
-    return config
-  },
+    },
+    resolve: {
+      ...config.resolve,
+      extensions: [...(config.resolve.extensions || []), ".ts", ".tsx"],
+    },
+  }),
 }
