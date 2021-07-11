32a8e7133f27131b6bb02ead21556342b03dcd4c
Kazeev <misha_kazeev@mail.ru>
2021-07-11 17:41:14 +0300

    feat: Сделал стиль как в store.rosbank

--- packages/slider/src/Slider.tsx
+++ packages/slider/src/Slider.tsx
@@ -1,5 +1,4 @@
 import React from "react"
-import "rc-slider/assets/index.css"
 import Slider from "rc-slider"
 import { SliderProps } from "rc-slider/lib/Slider"
 
@@ -9,28 +8,27 @@ interface SliderInterface extends React.InputHTMLAttributes<HTMLInputElement> {
   marks?: any
 }
 
-export const SliderWrap = React.forwardRef<
-  React.ComponentClass<SliderProps>,
-  SliderInterface
->(({ handle, step, marks, ...props }, ref) => {
-  return (
-    <>
-      <Slider
-        {...props}
-        handle={handle}
-        step={step}
-        marks={marks}
-        trackStyle={{
-          backgroundColor: "#e53935",
-        }}
-        dotStyle={{
-          borderColor: "#e53935",
-        }}
-        handleStyle={{
-          backgroundColor: "#e53935",
-          borderColor: "#e53935",
-        }}
-      />
-    </>
-  )
-})
+export const SliderWrap = React.forwardRef<SliderProps, SliderInterface>(
+  ({ handle, step, marks, ...props }, ref) => {
+    return (
+      <>
+        <Slider
+          {...props}
+          handle={handle}
+          step={step}
+          marks={marks}
+          trackStyle={{
+            backgroundColor: "#e53935",
+          }}
+          dotStyle={{
+            borderColor: "#e53935",
+          }}
+          handleStyle={{
+            backgroundColor: "#e53935",
+            borderColor: "#e53935",
+          }}
+        />
+      </>
+    )
+  },
+)
--- packages/slider/src/Sliderhandle.tsx
+++ packages/slider/src/Sliderhandle.tsx
@@ -1,13 +1,7 @@
 import React from "react"
-import "rc-slider/assets/index.css"
+import "./index.css"
 import Slider, { SliderTooltip } from "rc-slider"
-
 const { Handle } = Slider
-
-const point = {
-  background: "#e53935",
-  borderRadius: "50%",
-}
 
 export const handle = (props: any) => {
   const { value, dragging, index, ...restProps } = props
@@ -16,7 +10,6 @@ export const handle = (props: any) => {
       prefixCls="rc-slider-tooltip"
       overlay={`${value}`}
       visible={dragging}
-      overlayInnerStyle={point}
       placement="top"
       key={index}
     >
