10f69c94e89f1e4ef924810979fda2b581b0f25c
Kazeev <misha_kazeev@mail.ru>
2021-07-11 18:40:48 +0300

    feat: Сделал стиль как в store.rosbank

--- packages/slider/src/Slider.tsx
+++ packages/slider/src/Slider.tsx
@@ -2,33 +2,27 @@ import React from "react"
 import Slider from "rc-slider"
 import { SliderProps } from "rc-slider/lib/Slider"
 
-interface SliderInterface extends React.InputHTMLAttributes<HTMLInputElement> {
-  handle?: any
-  step?: any
-  marks?: any
-}
+interface SliderInterface extends SliderProps {}
 
-export const SliderWrap = React.forwardRef<SliderProps, SliderInterface>(
-  ({ handle, step, marks, ...props }, ref) => {
-    return (
-      <>
-        <Slider
-          {...props}
-          handle={handle}
-          step={step}
-          marks={marks}
-          trackStyle={{
-            backgroundColor: "#e53935",
-          }}
-          dotStyle={{
-            borderColor: "#e53935",
-          }}
-          handleStyle={{
-            backgroundColor: "#e53935",
-            borderColor: "#e53935",
-          }}
-        />
-      </>
-    )
-  },
-)
+export const SliderWrap = React.forwardRef<
+  React.ComponentClass,
+  SliderInterface
+>(({ ...props }, ref) => {
+  return (
+    <>
+      <Slider
+        {...props}
+        trackStyle={{
+          backgroundColor: "#e53935",
+        }}
+        dotStyle={{
+          borderColor: "#e53935",
+        }}
+        handleStyle={{
+          backgroundColor: "#e53935",
+          borderColor: "#e53935",
+        }}
+      />
+    </>
+  )
+})
